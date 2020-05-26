7237a7d0721ade559b1fb41db3407d2dfe787aa8
Hidetake Iwata <int128@gmail.com>
2020-05-26 15:11:38 +0900

    Bump version to v1.0.1

--- kauthproxy.rb
+++ kauthproxy.rb
@@ -1,9 +1,9 @@
 class Kauthproxy < Formula
   desc "A kubectl plugin to forward a local port to a pod or service via authentication proxy"
   homepage "https://github.com/int128/kauthproxy"
-  url "https://github.com/int128/kauthproxy/releases/download/v1.0.0/kauthproxy_darwin_amd64.zip"
-  version "v1.0.0"
-  sha256 "217597c2c2e18f336f6d11b6d76982ada426e420a8786f092407adec9962e6e0"
+  url "https://github.com/int128/kauthproxy/releases/download/v1.0.1/kauthproxy_darwin_amd64.zip"
+  version "v1.0.1"
+  sha256 "096aaa92eb423a7b40b18e034d13bb13bdd68266535369900db32b8ec415cb07"
   def install
     bin.install "kauthproxy" => "kauthproxy"
     ln_s bin/"kauthproxy", bin/"kubectl-auth_proxy"
